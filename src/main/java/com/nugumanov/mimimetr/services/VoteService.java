89af3e35b96367ffd3de5451099aa659a79918c2
OneilN <aizatjedisg@gmail.com>
2022-09-24 12:38:33 +0300

    Removed unused import

--- src/main/java/com/nugumanov/mimimetr/services/VoteService.java
+++ src/main/java/com/nugumanov/mimimetr/services/VoteService.java
@@ -4,7 +4,6 @@ import com.nugumanov.mimimetr.models.Cat;
 import com.nugumanov.mimimetr.models.Pair;
 import com.nugumanov.mimimetr.repositories.CatsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.annotation.SessionScope;
